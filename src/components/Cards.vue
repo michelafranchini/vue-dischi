11e5d6d07e6ddbf767b8aee08ddbd74a84af908e
michelafranchini <michela.franchini.91@gmail.com>
2021-06-09 18:13:58 +0200

    Bonus loader

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,29 +1,32 @@
 <template>
-    <section class="dark_container"> 
-        <div class="album">
+    <section class="dark_container" > 
+        <div class="album" v-if="!loading">
             <div class="card_container"
             v-for="card in cards" 
             :key="card.id">
                 <Card :item="card" />
             </div> 
         </div>
-      
+        <Loading v-else />
     </section>
 </template>
 
 <script>
 import Card from '../components/Card.vue';
+import Loading from '../components/Loading.vue'
 import axios from 'axios'; 
 
 export default {
     name: "Cards",
     components: {
-        Card
+        Card, 
+        Loading
     },
     data () {
         return {
             api: "https://flynn.boolean.careers/exercises/api/array/music", 
             cards: [], 
+            loading: true, 
         }
     }, 
     created() {
@@ -33,6 +36,11 @@ export default {
                 (result) => {
                     // console.log(result.data);
                     this.cards = result.data.response; 
+
+                    setTimeout( () => {
+                        this.loading = false; 
+                    }, 5000
+                    )
                 }
             )
     }
@@ -44,11 +52,13 @@ export default {
     .dark_container {
         padding: 50px;
         background-color: $colorDarkGrey;
+        padding: 20px 0;
+        height: 100vh;
     }
     .album {
-        width: 70%;
-        margin: auto;
+        width: 62%;
         height: 100%;
+        margin: auto;
         display: flex;
         flex-wrap: wrap;
         
---
+++ src/components/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="loader">
+        <h2>Aspetta solo qualche secondo</h2>
+        <img src="../img/loading-pacman.svg" alt="">
+        <h2>Tra poco potrai ascoltare la tua musica preferita senza attese</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Loading", 
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+    .loader {
+        position: fixed;
+        top: 0;  
+        left: 0; 
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column ;
+        justify-content: center;
+        align-items: center;
+        background-color: $colorDarkGrey;
+
+        h2{
+            display: block;
+            font-size: 30px;
+            text-transform: uppercase;
+            color: white; 
+        }
+    }
+</style>
